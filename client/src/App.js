c8db914e363efa197f90c296fbe458b1858bcf41
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-07-05 19:06:59 +0700

    connect mongodb / add style post form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form.js';
+import tokyo from './images/Tokyo.jpg';
+import useStyles from './styles.js';
 
 const App = () => {
+    const classes = useStyles();
+
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>My WebApp</Typography>
+                <img className={classes.image} src={tokyo} alt='tokyo' height='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
