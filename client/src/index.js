9939448247127ca16ed89879777b1d39297651b8
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-07-07 18:49:13 +0700

    createStore edited

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore, applyMiddleware, compose } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+// import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'; 
 import reducers from './reducers';
 
 import App from './App';
 
-const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
