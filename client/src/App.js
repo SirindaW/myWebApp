7c27bf5aa9cde73b22569cc5763dd80b6d3f83b1
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-07-13 16:11:23 +0700

    chore: ignore .env

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar.js';
 import Home from './components/Home/Home.js';
+import Auth from './components/Auth/Auth.js';
 
 const App = () => {
     return(
