c407b948e8e193871d2b71c14d8c0fb18d2a6373
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-07-15 19:28:08 +0700

    feat: implement google login logout at frontend

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,6 @@ export default (state = { authData: null }, action) => {
     switch (action.type) {
         case AUTH:
             localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
-            console.log(state);
             return { ...state, authData: action?.data };
         case LOGOUT:
             localStorage.clear();
