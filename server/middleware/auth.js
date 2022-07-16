84efb3511b82eda233b12df69647010082750fd5
Sirinda Wangfaen <74582141+SirindaW@users.noreply.github.com>
2022-07-16 17:27:20 +0700

    Feat: add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+    try {
+        // middleware is for check if user's token is valid then use next() to let user do the action (ex. like, delete post)
+        console.log(req.headers.authorization);
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500; // >=500 is GoogleAuth
+
+        let decodedData;
+
+        if(token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'test'); // secondary argument must be the same as secret string when create token 
+
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token); // use decode cause don't need the secret
+
+            req.userId = decodedData?.sub; // sub is google's specific id (different for every google user)
+        }
+
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
+export default auth;
